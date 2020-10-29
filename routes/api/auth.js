fd3a310488ffaa0a8b8fe8ac794de00af61fad57
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-10-28 20:04:26 -0400

    craeted profile model

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,7 +8,7 @@ const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 
 // @route   GET api/auth
-// @desc    Test route
+// @desc    Get user route
 // @access  Public
 router.get('/', auth, async (req, res) => {
   try {
